e729b288a3f85167a871402b4ff8c1f7d07ff659
Abrahan-Eagle <thehackeroffire@gmail.com>
2024-10-16 19:20:57 -0400

    1.4.1

--- lib/x.dart
+++
@@ -1,5 +0,0 @@
-     body: Center(
-        child: _selectedLevel == 1
-            ? const OtherScreen() // Cambia a la pantalla de Other si se selecciona el nivel 1
-            : Center(child: Text('Contenido del nivel $_selectedLevel')),
-      ),
